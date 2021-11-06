b08bb5e56b49c7cfdb938aba7f86955a444288c7
colson <colson@cogfactory.net>
2021-11-06 00:08:29 -0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+    let name = 'world';
+  let envVariable = process.env.foo;
+</script>
+
+<style>
+    h1{
+        color: tomato
+    }
+</style>
+
+<h1>Hello {name}!</h1>
+<p>env variable: {envVariable}</p>
